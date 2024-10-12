27be4ed919282fe50d2ecdea19c2664a7c8208b3
xynurm <nyakridha24@gmail.com>
2024-10-13 04:56:55 +0700

    handle pagination to get all of lists

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ import requests
 username = 'your-username'
 token = 'your-personal-access-token'
 
-def get_followers():
-    url = f'https://api.github.com/users/{username}/followers'
-    response = requests.get(url, auth=(username, token))
-    return [user['login'] for user in response.json()]
+following_url = f'https://api.github.com/users/{username}/following'
+followers_url = f'https://api.github.com/users/{username}/followers'
 
-def get_following():
-    url = f'https://api.github.com/users/{username}/following'
-    response = requests.get(url, auth=(username, token))
-    return [user['login'] for user in response.json()]
+def get_user_list(url):
+    users = []
+    while url:
+        response = requests.get(url, auth=(username, token))
+        if response.status_code == 200:
+            users.extend(user['login'] for user in response.json())
+            # Check if there is a next page
+            url = response.links.get('next', {}).get('url')
+        else:
+            print(f"Failed to retrieve data: {response.status_code}")
+            break
+    return users
 
-followers = set(get_followers())
-following = set(get_following())
+# Get lists of followers and following
+following = get_user_list(following_url)
+followers = get_user_list(followers_url)
 
-# People you are following who are not following you back
-not_following_back = following - followers
+# Find users who are not following back
+not_following_back = [user for user in following if user not in followers]
 
-print("People who did not follow back:")
+# Display the results
+print("Users not following you back:")
 for user in not_following_back:
     print(user)
+
+print(f"Total non-followers: {len(not_following_back)}")
